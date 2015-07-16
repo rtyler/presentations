06b17f9847057cb6fab76c845fbe041519f0d402
R. Tyler Croy <tyler@monkeypox.org>
2015-07-16 14:47:54 -0700

    Use the jruby-gradle jar plugin to make a self-contained demo

---
+++ jruby-gradle-java-powertools/example-project/main.rb
@@ -0,0 +1,12 @@
+require 'java'
+
+java_import 'com.github.rtyler.presentations.Demo'
+
+
+puts "> Running with Ruby: #{RUBY_VERSION} on #{RUBY_PLATFORM}"
+
+message = "Hello JRubyConf EU"
+puts "> sending echo message:  #{message}"
+puts
+
+puts Demo.new.echo(message)
